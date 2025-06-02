e51f4407f332667d9271f348afde5e3c45b06b69
Sunil Khetri <sunilkhetri05@gmail.com>
2025-06-02 16:01:16 +0530

    update

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,19 +1,19 @@
+// client/config-overrides.js //  
 const webpack = require("webpack");
 
 module.exports = function override(config) {
   config.resolve.fallback = {
-    ...config.resolve.fallback,
     buffer: require.resolve("buffer"),
-    process: require.resolve("process/browser"),
   };
 
   config.plugins = [
     ...config.plugins,
     new webpack.ProvidePlugin({
       Buffer: ["buffer", "Buffer"],
-      process: "process/browser",
     }),
   ];
 
   return config;
 };
+
+//Webpack 5 issue during versell deploy — it no longer provides polyfills for Node.js core modules like buffer, stream. created this file
